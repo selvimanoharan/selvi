46b8869ea9dc5930b2457e647753c08a6326e469
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2017-09-09 05:30:07 -0800

    Create s.c

---
+++ s.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int n,i;
+  int sum=0;
+  printf("enter the no");
+  scanf("%d",&n);
+  for(i=0;i<=n;++i)
+ {
+  sum=sum+i;
+  }
+ printf("sum of no is%d",sum);
+}
