254c20bb442c05ec181c1668cb32748bcf0a7d53
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2017-09-13 19:40:17 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int i,f,l;
+  printf("enter the first and last interval");
+  scanf("%d",&f,&l);
+  for(i=f;i<=l;i=i+2)
+
+    {
+      printf("%d",i);
+    }
+    return 0;
+}
