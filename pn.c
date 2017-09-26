77d6fcca757e465e4e6564e4b6777da6043a6aeb
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2017-09-26 06:12:38 -0800

    Update pn.c

--- pn.c
+++ pn.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int l, h, i, f;
-    printf("Enter two numbers(intervals): ");
+    int l, h, i, r;
+    printf("Enter two numbers ");
     scanf("%d %d", &l, &h);
 
-    printf("Prime numbers between %d and %d are: ", l, h);
+    printf("Prime no between %d and %d are: ", l, h);
 
     while (l < h)
     {
-        f = 0;
+        r = 0;
 
         for(i = 2; i <= l/2; ++i)
         {
             if(l % i == 0)
             {
-                f = 1;
+                r = 1;
                 break;
             }
         }
 
-        if (f == 0)
+        if (r == 0)
             printf("%d ", l);
 
         ++l;
