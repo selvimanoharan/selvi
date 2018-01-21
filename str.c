e98a513f838b3b8e4f368e69f91f515c2fcffec7
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-01-21 11:54:25 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,i;
+  char str[20];
+  printf("enter the string and number");
+  scanf("%s %d",&str[i],&a);
+  for(i=0;i<a;i++)
+  {
+    printf("%s\n",str);
+  }
+  return 0;
+}
