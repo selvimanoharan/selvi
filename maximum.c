65d85254fcbb360c4b3e4f02224dde8f9f0abae6
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-01-21 11:13:36 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],i,j,n,temp;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("list of element");
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+	    for(j=i+1;j<n;j++)
+	    {
+	        if(a[i]>a[j])
+	        {
+	        temp=a[i];
+	        a[i]=a[j];
+	        a[j]=temp;
+	        }
+	}
+	}
+	printf("maximam number is:");
+	printf("%d\n",a[n-1]);
+	return 0;
+}
