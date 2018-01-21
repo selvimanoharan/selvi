8f8c090d169723f6e8135bc89a307c2e3d63b630
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-01-21 12:21:26 +0530

    Create com.c

---
+++ com.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int a,i;
+  char str[20],str1[20];
+  printf("enter the string ");
+  scanf("%s",&str);
+  printf("enter the another string");
+  scanf("%s",str1);
+  printf("%s%s",str,str1);
+}
