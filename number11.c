8e9b143acd01a04c6a8e144e62ee384e409d861b
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-01-22 19:49:07 +0530

    Create number11.c

---
+++ number11.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int a,count=0;
+  printf("enter the number");
+  scanf("%d",&a);
+  if((a>=1)&&(a<=10))
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+  
+
+  
+}
