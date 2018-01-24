f2dc5ebca1138f1f043afb1ab33ba6609df31b81
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-01-24 16:07:06 +0530

    Create sum1.c

---
+++ sum1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int n,rem,result=0,i;
+  printf("enter the digit");
+  scanf("%d",&n);
+  for(i=0;i<=n;i++)
+  {
+  rem=n%10;
+  n=n/10;
+  result=result+rem;
+  }
+  printf("sum of the digit %d",result);
+  
+  return 0;
+}
