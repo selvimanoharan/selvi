c6ec9c7bdb7f98c9bede2382024f59b7a22e69e4
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-01-24 16:22:54 +0530

    Update dig.c

--- dig.c
+++ dig.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-  int a,count=0;
+  int n,count=0;
   printf("enter the the no");
-  scanf("%d",&a);
-  while(a!=0)
+  scanf("%d",&n);
+  while(n!=0)
   {
-  a=a/10;
+  n=n/10;
   count++;
 }
 printf("count is%d",count);
