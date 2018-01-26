fc0043ab82788b6d540e234693823823773a60ca
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-01-26 19:57:28 +0530

    Update f.c

--- f.c
+++ f.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int a=0, b=1,c,i,n;
+	int a=0, b=1,c,i,v;
 	printf("enter the number");
-	scanf("%d",&n);
+	scanf("%d",&v);
 	printf("fibonacci series");
-	for(i=0;i<n;i++)
+	for(i=0;i<v;i++)
 	{
 		printf("%d",a);
 	c=a+b;
