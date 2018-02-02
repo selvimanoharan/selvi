5e1632faa37d570d0e6497b8646cdc303510c4a1
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-02-02 19:45:54 +0530

    Create mul13.c

---
+++ mul13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int a,i;
+  printf("enter the number");
+  scanf("%d",&a);
+    if(a%13==0)
+    {
+      printf("yes");
+    }
+    else
+    {
+      printf("no");
+    }
+}
