c254a22613561bc2d6a2d4d36d940cd853d754b7
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-02-02 16:26:41 +0530

    Create medi.c

---
+++ medi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  int i,b,a;
+  char str[10];
+  printf("enter the string");
+  scanf("%s",str);
+  a=strlen(str);
+  b='*';
+  for(i=0;i<a;i++)
+{
+  str[a/2]=b;
+}
+  
+  for(i=0;i<a;i++)
+  {
+    printf("%c",str[i]);
+  }
+}
