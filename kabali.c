1b07b55e2c2eab37b523c24c63074d518e18f139
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-02-07 19:02:59 +0530

    Update kabali.c

--- kabali.c
+++ kabali.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 void main()
 {
-  int n,a,b,c,d,e,f,i;
+  int n,a[60],b[70],i;
   printf("enter no of lines");
   scanf("%d",&n);
+  printf("number of the kabali team and opponent");
   for(i=0;i<n;i++)
   {
-  printf("number of the kabali team member ");
-  scanf("%d %d",&a,&b);
-  printf("number of opponent team number");
-  scanf("%d %d",&c,&d);
-  e=b-a;
-  f=d-c;
-  printf("%d %d\n",e,f);
+  scanf("%d %d",&a[i],&b[i]);
   }
-
+  printf("difference between two team number");
+  for(i=0;i<n;i++)
+  {
+    printf("%d\n ",b[i]-a[i]);
+  }
+  
 }
