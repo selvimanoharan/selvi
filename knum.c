769de84369a43018fab5d0d703f5ea7e6a6821bd
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-02-09 10:35:13 +0000

    Create knum.c

---
+++ knum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int n,k,i,a[50];
+  printf("enter the num ");
+  scanf("%d %d",&n,&k);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    if(a[i]==k)
+    {
+      printf("%d",a[i]);
+    }
+  }
+return 0;
+}
+  
