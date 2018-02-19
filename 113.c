a1c2e89a37f3d4208070b48cf07e4c41a75e3a13
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-02-19 19:46:12 +0530

    Create 113.c

---
+++ 113.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+  int n,i,a[10],k,f=0;
+  
+  printf("enter the the no");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  printf("enter the number ");
+  scanf("%d",&k);
+  for(i=0;i<n;i++)
+  {
+    if(a[i]==k)
+    {
+    f++;
+    }
+  }
+  printf("%d",f);
+}
