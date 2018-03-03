241d290699eec49f5b3755bb444ce4d4f2bbcd77
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-03-03 19:30:12 +0530

    Create 103.c

---
+++ 103.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  int i,n;
+  char str[100];
+  printf("enter the word");
+  scanf("%[^\n]s",&str);
+  n=strlen(str);
+  for(i=0;i<n;i++)
+  {
+  str[0]-=32;
+  if(str[i]==' ')
+  {
+    str[i+1]-=32;
+  }
+  printf("%c",str[i]);
+  }
+}
