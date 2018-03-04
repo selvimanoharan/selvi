78c7edeb73a7bbf22eefa5071c2c0bc594592006
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-03-04 16:30:14 +0530

    Update 2.c

--- Hunter/2.c
+++ Hunter/2.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+void main()
+{
+  int n, a[10],i,j;
+  printf("enter the num");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+    
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+      if(a[i]>a[j])
+      {
+      int temp=a[i];
+      a[i]=a[j];
+      a[j]=temp;
+    }
+  }
+}
+for(i=n-1;i>=0;i--)
+{
+  printf("%d",a[i]);
+}
+}
