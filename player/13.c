3ebe318c567918b3ab81ed72a80692f602f9b536
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-03-12 16:13:56 +0530

    Create 13.c

---
+++ player/13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c, d,e;
+    printf("enter the two num");
+    scanf("%d %d",&a,&b);
+    c=a*a;
+    d=b*b;
+    e=c+d;
+    printf("sum of the square: %d",e);
+}
