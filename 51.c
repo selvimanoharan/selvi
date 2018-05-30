2b18eb173610c170970a7d25952386c9d123ac93
selvimanoharan <31443622+selvimanoharan@users.noreply.github.com>
2018-05-30 17:47:42 +0530

    Update 51.c

--- 51.c
+++ 51.c
@@ -12,9 +12,12 @@ void main()
     {
         for(j=i+1;j<=n;j++)
         {
+            if(a[i]>a[j])
+            {
             temp=a[i];
             a[i]=a[j];
             a[j]=temp;
+        }
         }
     }
     printf("%d",a[1]);
